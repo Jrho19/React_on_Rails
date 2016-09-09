e7b25be01fbd5ca38c75e6cf438bb1983eb8e2f1
John Rhoden <jrho19.jr@gmail.com>
2016-09-09 13:56:47 -0400

    EventTable and EventApplication adjustment and EventController has search method

--- app/assets/javascripts/components/event_table.js.jsx
+++ app/assets/javascripts/components/event_table.js.jsx
@@ -1,5 +1,10 @@
 var EventTable = React.createClass({
   render: function() {
+    var events = [];
+    this.props.events.forEach(function(event) {
+      events.push(<Event event={event}
+                         key={'event' + event.id}/>);
+    }.bind(this));
     return(
       <table className="table table-striped">
         <thead>
@@ -11,6 +16,7 @@ var EventTable = React.createClass({
           </tr>
         </thead>
         <tbody>
+          {events}
         </tbody>
       </table>
     )
