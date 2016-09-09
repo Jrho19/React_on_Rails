4b70b24863d1ec7121a78c004d08882b68be14fa
John Rhoden <jrho19.jr@gmail.com>
2016-09-09 17:38:08 -0400

    Search component is live and in service

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -17,12 +17,20 @@ var EventApplication = React.createClass({
       }
     });
   },
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
   render: function() {
     return(
     <div className="container">
       <div className="jumbotron">
         <h1>ReactJS Initiation</h1>
         <p>By J Rho</p>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <SearchForm handleSearch={this.handleSearch} />
+        </div>
       </div>
       <div className="row">
         <div className="col-md-12">
